1712e9a1c254ac811fece8bb26b7e32965fb0572
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2024-11-27 22:24:37 -0500

    se mejoro la generacion de pdf version 1.1

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const { PDFDocument, rgb } = require('pdf-lib');
+const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   openSecondWindow: () => ipcRenderer.send('open-second-window'),
@@ -9,23 +9,55 @@ contextBridge.exposeInMainWorld('electronAPI', {
   saveFile: (filePath, data) => ipcRenderer.invoke('save-file', filePath, data),
   createPdf: async (files) => {
     const pdfDoc = await PDFDocument.create();
-    let page = pdfDoc.addPage();
+    const page = pdfDoc.addPage();
     const { width, height } = page.getSize();
     const fontSize = 12;
-    let yPosition = height - fontSize * 2;
+    const margin = 50;
+    const maxLineWidth = width - margin * 2;
+    const lineHeight = fontSize * 1.5;
+    let yPosition = height - margin;
+
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    const splitTextIntoLines = (text, maxWidth) => {
+      const words = text.split(' ');
+      const lines = [];
+      let currentLine = '';
+
+      words.forEach(word => {
+        const testLine = currentLine ? `${currentLine} ${word}` : word;
+        const testLineWidth = font.widthOfTextAtSize(testLine, fontSize);
+        if (testLineWidth < maxWidth) {
+          currentLine = testLine;
+        } else {
+          lines.push(currentLine);
+          currentLine = word;
+        }
+      });
+
+      if (currentLine) {
+        lines.push(currentLine);
+      }
+
+      return lines;
+    };
 
     files.forEach((file, index) => {
-      if (yPosition < fontSize * 2) {
-        page = pdfDoc.addPage();
-        yPosition = height - fontSize * 2;
-      }
-      page.drawText(`${index + 1}. ${file}`, {
-        x: 50,
-        y: yPosition,
-        size: fontSize,
-        color: rgb(0, 0, 0),
+      const lines = splitTextIntoLines(`${index + 1}. ${file}`, maxLineWidth);
+      lines.forEach(line => {
+        if (yPosition < margin + lineHeight) {
+          page = pdfDoc.addPage();
+          yPosition = height - margin;
+        }
+        page.drawText(line, {
+          x: margin,
+          y: yPosition,
+          size: fontSize,
+          font: font,
+          color: rgb(0, 0, 0),
+        });
+        yPosition -= lineHeight;
       });
-      yPosition -= fontSize * 1.5;
     });
 
     const pdfBytes = await pdfDoc.save();
